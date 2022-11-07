95a0b1e2eb0f15d1540369f81a11c29290bf0032
Massimo Crippa <massimo.crippa@gmail.com>
2022-11-07 16:39:43 +0100

    node

--- Hands-on lab/lab-files/developer/scripts/create_and_seed_database.sh
+++ Hands-on lab/lab-files/developer/scripts/create_and_seed_database.sh
@@ -9,6 +9,6 @@ docker container run --name mongo --net fabmedical -p 27017:27017 -d mongo:4.0
 # Seed the mongo db
 cd ~/Fabmedical/content-init
 npm ci
-nodejs server.js
+node server.js
 
 echo "Build agent setup complete!"
